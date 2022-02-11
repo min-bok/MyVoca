975cb082df2d5c0d76a22f425af34b23846d2216
John Doe <leehy0782@naver.com>
2022-02-11 19:23:26 +0900

    add react-route-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './css/style.css'
 import Header from './components/Header'
 import DayList from './components/DayList'
+import Day from './components/Day'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
     <>
       <Header />
       <DayList />
+      <Day />
     </>
   );
 }
--- src/components/Day.js
+++ src/components/Day.js
@@ -1,3 +1,30 @@
-function Day() {}
+import dummy from '../DB/data.json'
+import style from '../css/Day.module.css'
+
+function Day() {
+    const day = 2;
+    const wordList = dummy.words.filter(word => word.day === day
+    )
+
+    return (
+        <table>
+            <tbody>
+                {wordList.map(word => (
+                <tr key={word.id}>
+                    <td>
+                        {word.cn}
+                    </td>
+                    <td>
+                        {word.pinyin}
+                    </td>
+                    <td>
+                        {word.kor}
+                    </td>
+                </tr>
+                ))}
+            </tbody>
+        </table>
+    )
+}
 
 export default Day;
