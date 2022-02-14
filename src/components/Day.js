ae9381facc060a560fa0663cfa6fc5a9ddbd229e
John Doe <leehy0782@naver.com>
2022-02-14 13:26:23 +0900

    update Day.js dummy to restAPI

--- src/components/Day.js
+++ src/components/Day.js
@@ -1,18 +1,28 @@
-import dummy from '../DB/data.json'
 import Word from '../components/Word'
 import { useParams } from 'react-router-dom'
+import { useEffect, useState } from 'react';
 
 function Day() {
     const day = useParams().day;
-    const wordList = dummy.words.filter(word => word.day === Number(day)
-    )
+    // const wordList = dummy.words.filter(word => word.day === Number(day)
+    const [words, setWords] = useState([]);
+
+    useEffect(() => {
+        fetch(`http://localhost:3001/words?day=${day}`)
+        .then(res => {
+            return res.json();
+        })
+        .then(data => {
+            setWords(data);
+    });
+}, [day]);
 
     return (
         <>
         <h2>Day {day}</h2>
         <table>
             <tbody>
-                {wordList.map(word => (
+                {words.map(word => (
                     <Word word={word} key={word.id}/>
                 ))}
             </tbody>
