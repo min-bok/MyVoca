4547a02edb9301440f0e11805942b3b67aa2a4eb
John Doe <leehy0782@naver.com>
2022-03-04 02:27:03 +0900

    update createDay.module.css

--- src/components/createDay.js
+++ src/components/createDay.js
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import useFetch from "../hooks/useFetch";
+import style from '../css/createDay.module.css'
 
 function CreateDay() {
     const days = useFetch('http://localhost:3001/days');
@@ -26,9 +27,9 @@ function CreateDay() {
     }
 
     return(
-        <div>
-            <h3>현재 일수  : {days.length}일</h3>
-            <button onClick={addDay}>Day 추가</button>
+        <div className={`${style.wrap}`}>
+            <h3 className={`${style.h3}`}>현재 일수  : {days.length}일</h3>
+            <button className={`${style.btn}`} onClick={addDay}>Day 추가</button>
         </div>
     )
 }
